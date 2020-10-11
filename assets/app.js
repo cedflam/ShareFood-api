3e6d323dc580ed26460767425a2f75a867dafc64
cedflam <cedflam@gmail.com>
2020-10-11 09:32:26 +0200

    React link to symfony

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,8 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
@@ -11,4 +13,11 @@ import './styles/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+console.log('Hello');
+
+const App = () => {
+    return <h1> Bonjour à tous !</h1>;
+};
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App/>, rootElement);
